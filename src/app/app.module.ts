08760177c4d3e953f7c122af39244fa07cdf8e69
Kent B Carlyle II <kentbcarlyle@gmail.com>
2019-05-26 13:04:33 -0400

    add 2 way data binding on input, add piping for table row fields

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { BrowserModule } from "@angular/platform-browser";
+import { FormsModule } from "@angular/forms";
 import { NgModule } from "@angular/core";
 
 import { AppComponent } from "./app.component";
@@ -6,7 +7,7 @@ import { ProductListComponent } from "./products/product-list.component";
 
 @NgModule({
   declarations: [AppComponent, ProductListComponent],
-  imports: [BrowserModule],
+  imports: [BrowserModule, FormsModule],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
