eeba5091927620bc524468d401cb8dac8aee32b6
Kent B Carlyle II <kentbcarlyle@gmail.com>
2019-05-26 12:52:36 -0400

    add images, light styling, directing to hide/show images, and chang btn text dynamically

--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -6,6 +6,9 @@ import { Component } from "@angular/core";
 })
 export class ProductListComponent {
   title: string = "Product List";
+  imageWidth: number = 50;
+  imageMargin: number = 2;
+  showImage: boolean = false;
   products: any[] = [
     {
       productId: 1,
@@ -30,4 +33,8 @@ export class ProductListComponent {
         "https://openclipart.org/image/300px/svg_to_png/58471/garden_cart.png"
     }
   ];
+
+  toggleImage(): void {
+    this.showImage = !this.showImage;
+  }
 }
