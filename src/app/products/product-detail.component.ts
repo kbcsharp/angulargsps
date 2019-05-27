8e1e25dc1498e21c9cab49d31696025e8f4d5bf7
Kent B Carlyle II <kentbcarlyle@gmail.com>
2019-05-27 17:19:39 -0400

    minimal prod detail styling, add get prod to replace hard code prod in prod detail

--- src/app/products/product-detail.component.ts
+++ src/app/products/product-detail.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from "@angular/core";
 import { IProduct } from "./product";
 import { ActivatedRoute, Router } from "@angular/router";
+import { ProductService } from "./product.service";
 
 @Component({
   templateUrl: "./product-detail.component.html",
@@ -9,24 +10,21 @@ import { ActivatedRoute, Router } from "@angular/router";
 export class ProductDetailComponent implements OnInit {
   title: string = "Product Detail";
   product: IProduct;
+  errorMessage: string;
 
   constructor(private route: ActivatedRoute,
-              private router: Router) {}
+              private router: Router,
+              private productService: ProductService) {}
 
   ngOnInit() {
     let id = +this.route.snapshot.paramMap.get("id");
     this.title += `: ${id}`;
-    this.product = {
-      productId: id,
-      productName: "Leaf Rake",
-      productCode: "GDN-0011",
-      releaseDate: "March 19, 2016",
-      description: "Leaf rake with 48-inch wooden handle",
-      price: 19.95,
-      starRating: 3.2,
-      imageUrl:
-        "https://openclipart.org/image/300px/svg_to_png/26215/Anonymous_Leaf_Rake.png"
-    };
+    this.productService.getProduct(id).subscribe(
+      product => {
+        this.product = product,
+        error => this.errorMessage = <any>error
+      }
+    );
   }
 
   onBack(): void {
