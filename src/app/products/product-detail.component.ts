b9c2cffa23c3c6f3e9d3527157b61a9d9429fed8
Kent B Carlyle II <kentbcarlyle@gmail.com>
2019-05-27 15:29:57 -0400

    add coded navigation back button in prod list

--- src/app/products/product-detail.component.ts
+++ src/app/products/product-detail.component.ts
@@ -1,17 +1,35 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 import { IProduct } from "./product";
+import { ActivatedRoute, Router } from "@angular/router";
 
 @Component({
-  templateUrl: './product-detail.component.html',
-  styleUrls: ['./product-detail.component.css']
+  templateUrl: "./product-detail.component.html",
+  styleUrls: ["./product-detail.component.css"]
 })
 export class ProductDetailComponent implements OnInit {
   title: string = "Product Detail";
   product: IProduct;
 
-  constructor() { }
+  constructor(private route: ActivatedRoute,
+              private router: Router) {}
 
   ngOnInit() {
+    let id = +this.route.snapshot.paramMap.get("id");
+    this.title += `: ${id}`;
+    this.product = {
+      productId: id,
+      productName: "Leaf Rake",
+      productCode: "GDN-0011",
+      releaseDate: "March 19, 2016",
+      description: "Leaf rake with 48-inch wooden handle",
+      price: 19.95,
+      starRating: 3.2,
+      imageUrl:
+        "https://openclipart.org/image/300px/svg_to_png/26215/Anonymous_Leaf_Rake.png"
+    };
   }
 
+  onBack(): void {
+    this.router.navigate(["/products"]);
+  }
 }
